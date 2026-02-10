047467cb6186326f396d3d6f525d6b0836e98172
harmless95 <213446016+harmless95@users.noreply.github.com>
2026-02-10 17:22:04 +0700

    Create TG bot install aiogram

---
+++ project_tg/main.py
@@ -0,0 +1,21 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TG_TOKEN = os.getenv("TG_TOKEN")
+
+bot = Bot(token=TG_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start(message: Message):
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
