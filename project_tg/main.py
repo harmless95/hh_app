b8d72c52f0e1514737f25fededad2be640ee14b2
harmless95 <213446016+harmless95@users.noreply.github.com>
2026-02-12 03:05:44 +0700

    fix message command start

--- project_tg/main.py
+++ project_tg/main.py
@@ -10,12 +10,17 @@ TG_TOKEN = os.getenv("TG_TOKEN")
 bot = Bot(token=TG_TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
 async def command_start(message: Message):
-    await message.answer(f"Hello, <b>{message.from_user.full_name}</b>!", parse_mode="HTML")
+    await message.answer(
+        f"Hello, <b>{message.from_user.full_name}</b>!", parse_mode="HTML"
+    )
+
 
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
