a80b07800b3541b88090310dde67be969d1fc955
harmles95 <213446016+harmless95@users.noreply.github.com>
2026-02-22 00:58:10 +0700

    changed the functionality of the work.

--- project_parsing/main.py
+++ project_parsing/main.py
@@ -1,5 +1,4 @@
 import asyncio
-import json
 import os
 import logging
 
@@ -10,12 +9,9 @@ from database.create_pool_db import db_pg
 
 logger = logging.getLogger("ParseData")
 
-text = "playwright"
-work_format = "REMOTE"
-items_on_page = "20"
-experience = "moreThan6"
 
-# URL = f"https://abakan.hh.ru/search/vacancy?text={text}&work_format={work_format}&items_on_page={items_on_page}&&experience={experience}"
+URL = f"https://hh.ru/search/vacancy?text=python&items_on_page=20"
+# URL_ID = "http://my_app/check/id"
 
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
@@ -25,24 +21,18 @@ redis_client = redis.Redis(
     port=6379,
     decode_responses=True,
 )
+name_channel = "list_id_vacancy"
+name_channel_duble = "duble_vacancy"
 
 
-async def check(id_vac, conn):
-    try:
-        query = """
-                    SELECT * FROM vacancy_data WHERE id_vacancy=$1
-                """
-        result_check = await conn.fetchrow(query, id_vac)
-    except Exception as e:
-        logger.error(f"Database connection failed: {e}")
-        return None
-    return result_check
-
-
-async def get_hh_page(page, url: str):
+async def get_hh_page(page, url):
+    data_list = await redis_client.smembers(name_channel)
+    data_duble = await redis_client.smembers(name_channel_duble)
 
     # Переходим по ссылке
     await page.goto(url, wait_until="domcontentloaded")
+
+    await asyncio.sleep(2)
 
     # Можно сделать скриншот, чтобы проверить, не вылезла ли капча
     await page.screenshot(path="./resource/hh_check.png")
@@ -50,66 +40,83 @@ async def get_hh_page(page, url: str):
     # Получаем HTML всей страницы
     content = await page.content()
 
+    # Проверяем на наличие вакансий
     vacancy = await page.locator('div[data-qa="vacancy-serp__vacancy"]').all()
-    print("Кол-во: ", len(vacancy))
-    result = []
-    result_id = []
-    pool = await db_pg.get_pool()
+    logger.info("Найдено %s вакансий", len(vacancy))
+    print("кол-во вакнсий", len(vacancy))
+    list_vacancy = []
+    list_id_vacancy = []
+    list_duble_vacancy = []
 
-    async with pool.acquire() as conn:
-        for card in vacancy:
-            id_vac = await card.locator(
-                'div[class="vacancy-card--n77Dj8TY8VIUF0yM font-inter"]'
-            ).get_attribute("id")
-            print("id", id_vac)
-            result_id.append(id_vac)
-            check_db = await check(int(id_vac), conn)
-            if check_db:
-                continue
-            titles = await card.locator(
-                '[data-qa="serp-item__title-text"]'
-            ).text_content()
-            company_name = await card.locator(
-                '[data-qa="vacancy-serp__vacancy-employer"]'
-            ).text_content()
-            link_vacancy = await card.locator(
-                '[data-qa="serp-item__title"]'
-            ).get_attribute("href")
+    for card in vacancy:
+        id_vac = await card.locator(
+            'div[class="vacancy-card--n77Dj8TY8VIUF0yM font-inter"]'
+        ).get_attribute("id")
+        if not id_vac or id_vac in data_list:
+            continue
 
-            result.append(
-                {
-                    "id_vacancy": int(id_vac),
-                    "Название вакансии": titles,
-                    "Название компании": company_name,
-                    "Сcылка": link_vacancy,
-                }
+        tit = card.locator('[data-qa="serp-item__title-text"]')
+        if await tit.count() > 0:
+            titles = await tit.text_content()
+        else:
+            titles = "Название вакансии не указанно"
+
+        comp_name = card.locator('[data-qa="vacancy-serp__vacancy-employer"]')
+        if await comp_name.count() > 0:
+            company_name = await comp_name.text_content()
+        else:
+            company_name = "Название компании не указанно"
+        duble_vac = f"{titles}|{company_name}"
+        if duble_vac in data_duble or duble_vac in list_duble_vacancy:
+            continue
+        list_duble_vacancy.append(duble_vac)
+
+        link_vacancy = await card.locator('[data-qa="serp-item__title"]').get_attribute(
+            "href"
+        )
+
+        list_vacancy.append(
+            {
+                "id_vacancy": int(id_vac),
+                "Название вакансии": titles,
+                "Название компании": company_name,
+                "Сcылка": link_vacancy,
+            }
+        )
+        list_id_vacancy.append(id_vac)
+
+    if list_id_vacancy:
+        await redis_client.sadd(
+            name_channel,
+            *list_id_vacancy,
+        )
+    if list_duble_vacancy:
+        await redis_client.sadd(name_channel_duble, *list_duble_vacancy)
+    return list_vacancy
+
+
+async def get_link(page, list_vacancy: list):
+    for idx, item in enumerate(list_vacancy):
+        try:
+            logger.info("Загруженна вакансия: %s", (idx + 1))
+            print("загружен", idx)
+            await page.goto(
+                item.get("Сcылка"),
+                wait_until="domcontentloaded",
+                timeout=15000,
             )
-        for idx, item in enumerate(result):
-            try:
-                print(f"Загруженна вакансия: {idx + 1}")
-                await page.goto(
-                    item.get("Сcылка"),
-                    wait_until="domcontentloaded",
-                    timeout=15000,
-                )
-                item["skills"] = await page.locator(
-                    '[data-qa="skills-element"]'
-                ).all_text_contents()
-                logger.info("HOST Reddis: %s", REDIS_HOST)
-                await redis_client.publish(
-                    "vacancy_hh",
-                    json.dumps(item, ensure_ascii=False),
-                )
-            except Exception as e:
-                print(f"Ошибка при загрузке {item.get('Сcылка')}: {e}")
-                item["skills"] = []
-        return result
+            item["skills"] = await page.locator(
+                '[data-qa="skills-element"]'
+            ).all_text_contents()
+        except Exception as e:
+            print(f"Ошибка при загрузке {item.get('Сcылка')}: {e}")
+            item["skills"] = []
+    return list_vacancy
 
 
 async def main():
     page_num = 0
-    URL = await db_pg.check_url(id_telegram=5103681164)
-    print("url:", URL)
+    list_content = []
     async with async_playwright() as p:
         # Запускаем браузер (headless=True — без открытия окна)
         browser = await p.chromium.launch(headless=True)
@@ -121,14 +128,16 @@ async def main():
 
         page = await context.new_page()
 
-        while True:
-            url = URL + f"&page={page_num}"
+        while page_num < 3:
+            url = URL + f"&page={page_num}&order_by=publication_time"
             content = await get_hh_page(page, url)
             if not content:
                 break
-            await db_pg.pool_async(content)
+            list_content.extend(content)
             page_num += 1
             await asyncio.sleep(1)
+        list_data = await get_link(page=page, list_vacancy=list_content)
+        await db_pg.pool_async(list_data)
 
         await browser.close()
 
