159be4038c17b75caa409b27aa5b38e921d9a1a7
harmles95 <213446016+harmless95@users.noreply.github.com>
2026-02-23 01:31:56 +0700

    We process messages from Telegram and, based on them, display vacancies from the database.

--- project_tg/main.py
+++ project_tg/main.py
@@ -2,63 +2,77 @@ import asyncio
 import json
 import os
 import logging
+import httpx
 
 from aiogram import Bot, Dispatcher, html, F
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-import redis.asyncio as redis
+from aiogram.fsm.context import FSMContext
+
+# import redis.asyncio as redis
+
 
 TG_TOKEN = os.getenv("TG_TOKEN")
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+# REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=6379,
-    decode_responses=True,
-)
+# redis_client = redis.Redis(
+#     host=REDIS_HOST,
+#     port=6379,
+#     decode_responses=True,
+# )
 logger = logging.getLogger("TG_app")
 
 bot = Bot(token=TG_TOKEN)
 dp = Dispatcher()
 active_users = set()
+url_app = "http://fastapi_app:8000/v1/data/tg/"
 
 
-async def redis_listen():
-    pub_sub = redis_client.pubsub()
-    await pub_sub.subscribe("vacancy_hh")
-    try:
-        async for redis_msg in pub_sub.listen():
-            logger.info("Слушаю")
-            if redis_msg["type"] == "message":
-                logger.info(redis_msg["data"])
-                data = json.loads(redis_msg["data"])
-                text = (
-                    f"🔥 <b>Новая вакансия!</b>\n\n"
-                    f"💼 {data.get('Название вакансии')}\n"
-                    f"🏢 {data.get('Название компании')}\n"
-                    f"🔗 <a href='{data.get('Сcылка')}'>Открыть на HH</a>"
-                )
-                await asyncio.sleep(0.5)
-                for user_id in active_users:
-                    try:
-                        await bot.send_message(user_id, text, parse_mode="HTML")
-                    except Exception as e:
-                        logger.error(f"Не смог отправить сообщение {user_id}: {e}")
-    finally:
-        await pub_sub.close()
+async def get_vac(message: Message):
+    text = message.text
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url=url_app, json=text)
+        response.raise_for_status()
+        vacancies = response.json()
+        for item in vacancies[:10]:
+            text_vac = (
+                f"🔹 <b>{item.get('name_vacancy', 'Без названия')}</b>\n"
+                f"🏢 Компания: {item.get('name_company', 'Не указана')}\n"
+                f"🔗 <a href='{item.get('link', '#')}'>Перейти к вакансии</a>"
+            )
+            await message.answer(
+                text_vac,
+                parse_mode="HTML",
+            )
+            await asyncio.sleep(1)
 
 
 @dp.message(CommandStart())
 async def command_start(message: Message):
     active_users.add(message.from_user.id)
     await message.answer(
-        f"Привет, <b>{message.from_user.full_name}</b>! Теперь я буду присылать тебе вакансии из Redis.",
+        f"Привет, <b>{message.from_user.full_name}</b>! Укажите по каким навыкам искать?",
         parse_mode="HTML",
     )
 
 
+@dp.message(Command("stop"))
+async def command_stop(
+    message: Message,
+    state: FSMContext,
+):
+    await state.clear()
+    await message.answer("Все процессы остановились")
+
+
+@dp.message(F.text)
+async def command_text(message: Message):
+    await message.answer(f"Ищу вакансии по навыку: {message.text}...")
+    await get_vac(message=message)
+
+
 async def main():
-    asyncio.create_task(redis_listen())
+    # asyncio.create_task(redis_listen())
     await dp.start_polling(bot)
 
 
