ce754a111a580f6dfb46e2f299c4b3c2cc3664d2
harmles95 <213446016+harmless95@users.noreply.github.com>
2026-03-07 21:33:55 +0700

    corrected connect db

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import Literal
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 
 from .log_settings import setup_logger
 
@@ -10,6 +10,26 @@ from .log_settings import setup_logger
 LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 # fmt: on
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class Redis(BaseModel):
+    host: str
+
+
+class PostgresDB(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class LoggingConfig(BaseModel):
@@ -34,6 +54,9 @@ class Settings(BaseSettings):
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
     )
+    redis: Redis
+    db: PostgresDB
+    my_logger: LoggingConfig = LoggingConfig()
 
 
 setting = Settings()
--- app/core/model/base.py
+++ app/core/model/base.py
@@ -1,16 +1,10 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import MetaData
 
-naming_convention: dict[str, str] = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_N_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
-}
+from core.config import setting
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    metadata = MetaData(naming_convention=naming_convention)
+    metadata = MetaData(naming_convention=setting.db.naming_convention)
