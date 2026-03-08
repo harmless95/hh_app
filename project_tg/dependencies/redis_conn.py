f1d991c0d58aba30df1fe43afd2340ec7190e742
harmles95 <213446016+harmless95@users.noreply.github.com>
2026-03-09 01:24:55 +0700

    adjusted for radish on a remote server.

--- project_tg/dependencies/redis_conn.py
+++ project_tg/dependencies/redis_conn.py
@@ -1,11 +1,14 @@
 import redis.asyncio as redis
+import ssl
 import os
 
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PASSWORD = os.getenv("REDIS__PASSWORD")
 redis_channel = "tasks_vacancy"
+FULL_REDIS_URL = f"rediss://default:{REDIS_PASSWORD}@{REDIS_HOST}:6379/0"
 
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=6379,
+redis_client = redis.from_url(
+    FULL_REDIS_URL,
+    ssl_cert_reqs=ssl.CERT_NONE,
     decode_responses=True,
 )
